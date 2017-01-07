7719b6ad9c517c36f4a7652d7a4f55ca2d448a4a
zzt <zengzt93@gmail.com>
2017-01-07 09:43:46 +0800

    reformat and finish test;

--- chapter11/csapp.c
+++ chapter11/csapp.c
@@ -535,10 +535,11 @@ ssize_t rio_writen(int fd, void *usrbuf, size_t n) {
     while (nleft > 0) {
         if ((nwritten = write(fd, bufp, nleft)) <= 0) {
             if (errno == EINTR) {
-
                 /* Interrupted by sig handler return */
                 nwritten = 0;    /* and call write() again */
             } else if (errno == EPIPE) {
+                // another end is not request anymore and close the connection,
+                // so we pretend that we finished the write by `return n` directly
                 return n;
             } else {
                 return -1;       /* errno set by write() */
